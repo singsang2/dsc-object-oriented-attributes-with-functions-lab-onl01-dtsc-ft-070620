ccb94dfa92a3f3d822b22ad99e13f14d44eb7f6d
Sung Bae <singsang2@gmail.com>
2020-09-08 12:24:30 -0400

    completed

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+
+    def grade(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        for key in self.roster.keys():
+            self.roster[key].sort()
+        return self.roster
